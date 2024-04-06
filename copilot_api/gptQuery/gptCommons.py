d5594b27349e7a3f53d8a129e38d75f789ac0e95
asish012 <asish012@gmail.com>
2024-04-07 00:40:09 +0200

    added try-catch

--- copilot_api/gptQuery/gptCommons.py
+++ copilot_api/gptQuery/gptCommons.py
@@ -7,21 +7,28 @@ class GPTQuery:
     CLIENT = OpenAI(api_key=OPENAI_API_KEY)
     PROMPT = None
 
-    def __init__(self, system_prompt, user_prompt):
+    def __init__(self, system_prompt, user_prompt, tokens=2000, temperature=1):
         self._system_prompt = system_prompt
         self._user_prompt = user_prompt
         self._result = None
+        self._max_tokens = tokens
+        self._temperature = temperature
         self.generate()
 
     def generate(self):
-        completion = self.CLIENT.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": self._system_prompt},
-                {"role": "user", "content": self._user_prompt},
-            ],
-        )
-        self._result = completion.choices[0].message.content
+        try:
+            completion = self.CLIENT.chat.completions.create(
+                model="gpt-3.5-turbo",
+                messages=[
+                    {"role": "system", "content": self._system_prompt},
+                    {"role": "user", "content": self._user_prompt},
+                ],
+                max_tokens=self._max_tokens,
+                temperature=self._temperature
+            )
+            self._result = completion.choices[0].message.content
+        except Exception as e:
+            raise Exception(f'GPTQuery Error: {str(e)}')
 
     def get_result(self):
         return self._result
